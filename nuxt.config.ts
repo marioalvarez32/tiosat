3f1a4989ae9fdba9efdf2a92a0980ce11095a590
marioalvarez32 <33704981+marioalvarez32@users.noreply.github.com>
2025-09-15 20:40:45 -0500

    ci(Initial boilerplate configuration): Going back to the nuxt eslint module since it does keep things simpler and they allow eslint stylistic.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   modules: [
     'vuetify-nuxt-module',
     '@pinia/nuxt',
     '@nuxtjs/supabase',
-    '@nuxt/eslint'
+    '@nuxt/eslint',
   ],
+  devtools: { enabled: true },
   css: ['@mdi/font/css/materialdesignicons.css'],
-  vuetify: {
-    moduleOptions: {
-      /* module specific options */
+  eslint: {
+    config: {
+      stylistic: true,
     },
-    vuetifyOptions: './vuetify.config.ts' // <== you can omit it
   },
   runtimeConfig: {
     // Keys in here are ONLY available on the server-side.
@@ -25,11 +23,13 @@ export default defineNuxtConfig({
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseKey: process.env.SUPABASE_KEY,
-    }
+    },
   },
-  eslint: {
-    config: {
-      stylistic: true
-    }
-  }
+  compatibilityDate: '2025-07-15',
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: './vuetify.config.ts', // <== you can omit it
+  },
 })
